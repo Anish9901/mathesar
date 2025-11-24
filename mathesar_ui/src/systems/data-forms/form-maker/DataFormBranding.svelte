10e001b6ddbabada32d8b9d2e0af512ec5cfa18c
Ashwath <ashwath.p.s.138@kalvium.community>
2025-11-24 23:37:25 +0530

    ran prettier

--- mathesar_ui/src/systems/data-forms/form-maker/DataFormBranding.svelte
+++ mathesar_ui/src/systems/data-forms/form-maker/DataFormBranding.svelte
@@ -19,18 +19,18 @@
 
 <style lang="scss">
   .form-branding a {
-    color: var(--color-fg-base); 
+    color: var(--color-fg-base);
     display: inline-flex;
     align-items: center;
     gap: 0.25rem;
     text-decoration: none;
-    border-bottom: 2px solid transparent; 
+    border-bottom: 2px solid transparent;
     fill: currentColor;
     stroke: currentColor;
   }
 
   .form-branding a:hover,
   .form-branding a:focus {
-    border-bottom-color: currentColor; 
+    border-bottom-color: currentColor;
   }
 </style>
