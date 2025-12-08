147a883b8720946a53161b1ed447589ce3f5900b
zackdotcat <git@zack.cat>
2025-12-08 13:07:24 -0500

    simplify implementation

--- mathesar_ui/src/systems/data-forms/form-maker/DataFormBranding.svelte
+++ mathesar_ui/src/systems/data-forms/form-maker/DataFormBranding.svelte
@@ -18,19 +18,20 @@
 </div>
 
 <style lang="scss">
+  .form-branding {
+    padding: var(--sm1);
+  }
+
   .form-branding a {
     color: var(--color-fg-base);
     display: inline-flex;
     align-items: center;
     gap: 0.25rem;
     text-decoration: none;
-    border-bottom: 2px solid transparent;
-    fill: currentColor;
-    stroke: currentColor;
   }
 
   .form-branding a:hover,
   .form-branding a:focus {
-    border-bottom-color: currentColor;
+    border-bottom: 1.5px solid var(--color-brand);
   }
 </style>
